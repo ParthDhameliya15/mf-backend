c702a7d4554870e29b3dbf36d1a90a1c74e35c7c
parth <parthdhameliya266@gmail.com>
2025-06-05 19:37:11 +0530

    Fix mongo connection

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ const connectToDatabase = async () => {
   if (isConnected) return;
 
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     isConnected = true;
     console.log("Connected to MongoDB");
   } catch (error) {
