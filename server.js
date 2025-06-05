d89ae72017a6c757ed87dd8965c7edcfcb4b0f68
parth <parthdhameliya266@gmail.com>
2025-06-05 19:55:44 +0530

    fix cors configuration

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://mf-frontend-nu.vercel.app/", "http://localhost:5173"],
+    origin: ["https://mf-frontend-nu.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "x-api-key"]
+    allowedHeaders: ["Content-Type", "Authorization", "x-api-key"],
+    credentials: true
   })
 );
 
